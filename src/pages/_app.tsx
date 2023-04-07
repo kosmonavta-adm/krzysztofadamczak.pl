3b30e910461c8518987edf13ea47ec56bf84735f
kosmonavta-adm <130152929+kosmonavta-adm@users.noreply.github.com>
2023-04-07 23:03:23 +0200

    Revert "K1 Add global styles and ssr support for styled-components"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,5 @@
 import type { AppProps } from 'next/app';
-import { GlobalStyles } from '../utils/Global.styles';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <GlobalStyles />
-            <Component {...pageProps} />
-        </>
-    );
+    return <Component {...pageProps} />;
 }
