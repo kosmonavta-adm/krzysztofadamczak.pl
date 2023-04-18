4fa30dc3c9fa3e916375aa2c436ee41b697b8c3a
Krzysztof Adamczak <krzadamczak@gmail.com>
2023-04-18 19:53:00 +0200

    KP-2 Finish hero section

--- src/components/commons/Button/Button.styles.js
+++ src/components/commons/Button/Button.styles.js
@@ -4,15 +4,17 @@ export const Button = styled.button`
     border: none;
     cursor: pointer;
     text-decoration: none;
-    background-color: #5048ff;
-    color: #fff;
-    padding: 10px 20px;
+    background-color: ${({ theme }) => theme.colors.purple[0]};
+    color: ${({ theme }) => theme.colors.white};
+    padding: 12px 24px;
     border-radius: 5px;
     transition: 0.3s cubic-bezier(0, 0, 0.25, 1);
     transition-property: background-color, transform, box-shadow;
     box-shadow: 0 5px 10px rgb(10 0 143 / 40%);
+    font-size: 16px;
+    font-family: ${({ theme }) => theme.font};
     :hover {
-        background-color: #3d33ff;
+        background-color: ${({ theme }) => theme.colors.purple[1]};
         transform: scale(0.98);
         box-shadow: 0 2px 9px rgb(10 0 143 / 50%);
     }
